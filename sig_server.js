ee651a2ac698204e82d700877e98908d66372398
Ubuntu <ubuntu@ip-172-31-39-155.ap-northeast-2.compute.internal>
2023-02-09 06:37:19 +0000

    removed unused lines

--- sig_server.js
+++ sig_server.js
@@ -11,26 +11,11 @@ var app = http.createServer(function(req, res) {
 
 var io = socketIO.listen(app);
 io.sockets.on('connection', function(socket) {
-
-  // convenience function to log server messages on the client
-  function log() {
-    var array = ['Message from server:'];
-    array.push.apply(array, arguments);
-    socket.emit('log', array);
-  }
   function findRoomsFromSocketId(socketId) {
     let rooms = Array.from(Object.keys(io.sockets.adapter.sids[socketId]));
     rooms = rooms.filter(room => room !== socketId);
     return rooms
   }
-
-
-  socket.on('message', function(message) {
-    log('Client said: ', message);
-    // for a real app, would be room-only (not broadcast)
-    socket.broadcast.emit('message', message);
-    console.log('message: ', message);  
-  });
   socket.on('camera join', function(room) {
     console.log('A camera has joined to room: ' + room);
     socket.join(room);
@@ -90,31 +75,6 @@ io.sockets.on('connection', function(socket) {
     console.log('Current Sids State: ', io.sockets.adapter.sids);
     console.log('Current Room State: ', io.sockets.adapter.rooms);
   });
-  /*
-  socket.on('create or join', function(room) {
-    console.log('create or join: ', room);  
-    log('Received request to create or join room ' + room);
-    socket.join(room);
-    
-    var clientsInRoom = io.sockets.adapter.rooms[room];
-    var numClients = clientsInRoom ? Object.keys(clientsInRoom.sockets).length : 0;
-    log('Room ' + room + ' now has ' + numClients + ' client(s)');
-    if (numClients === 0) {
-      socket.join(room);
-      log('Client ID ' + socket.id + ' created room ' + room);
-      socket.emit('created', room, socket.id);
-
-    } else if (numClients === 1 || numClients === 2) {
-      log('Client ID ' + socket.id + ' joined room ' + room);
-      io.sockets.in(room).emit('join', room);
-      socket.join(room);
-      socket.emit('joined', room, socket.id);
-      io.sockets.in(room).emit('ready');
-    } else { // max two clients
-      socket.emit('full', room);
-    }
-  });*/
-
   socket.on('ipaddr', function() {
     var ifaces = os.networkInterfaces();
     for (var dev in ifaces) {
